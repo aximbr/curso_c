e0e8dd5220890e54520fec7afaca21a20d1f633e
Jose P. Leitao <jose.p.leitao@gmail.com>
2021-12-10 20:05:28 -0300

    Aula 09 Hangman

---
+++ aula_09/src/aula_09.c
@@ -0,0 +1,93 @@
+/* Nono programa para estudar C */
+/* hangman game */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(){
+
+    srand(time(NULL));  //initialize a random seed
+
+    char guessWord [] [16] = {
+        "green",
+        "yellow",
+        "purple",
+        "windows",
+        "linux",
+        "apple"      
+    };
+
+    //create a random int for index
+    int randomIndex = rand() % 6; // create a random number from 0 to 6
+
+    //some variables for control
+    int numLives = 5;
+    int numCorrect = 0;
+    int oldCorrect = 0;
+    int indexLoop = 0;
+
+    int lengthOfWord = strlen(guessWord[randomIndex]);
+
+    int letterGuessed[8] = { 0, 0, 0, 0, 0, 0, 0, 0};
+
+    int quit = 0;
+
+    char guess[16];
+    char letterEntered;
+
+    printf("The word is %s \n\n", guessWord[randomIndex]); //DEBUG
+
+    //game loop
+    while( numCorrect < lengthOfWord){
+        printf("Enter a guess letter:");
+        printf("\nSo far number of correct letters are: %d\n", numCorrect);
+
+        fgets(guess,16, stdin);
+
+        if (strncmp(guess, "quit", 4) == 0){
+            quit = 1;
+            break;
+        }
+
+        letterEntered = guess[0];
+
+        printf("\nLetter Entered was %c\n", letterEntered);
+
+        oldCorrect = numCorrect;
+
+        for (indexLoop = 0; indexLoop < lengthOfWord; indexLoop++){
+            if (letterGuessed[indexLoop] == 1){
+                continue;
+            }
+            if (letterEntered == guessWord[randomIndex][indexLoop]) {
+                letterGuessed[indexLoop] = 1;
+                numCorrect++;
+            }
+        }
+        if (oldCorrect == numCorrect){
+            numLives--;
+            printf("Sorry, wrong guess!\n");
+            if (numLives == 0){
+                break;
+                }
+        }   
+            else {
+                printf("Correct guess!\n");
+            }
+        
+
+    }//while loop
+
+    if (quit == 1){
+        printf("\nUser quit early.\n");
+    } else if (numLives == 0){
+        printf("\nYou lose. The word was: %s\n\n", guessWord[randomIndex]);
+    } else {
+        printf("\nCongrats, you Win! \n");
+    }
+    
+
+    return 0;
+}
