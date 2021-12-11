bc2083425792fe9436df6fd73d6b3d128e3e6edf
Jose P. Leitao <jose.p.leitao@gmail.com>
2021-12-11 08:24:20 -0300

    Include new comments on Aula_09

--- aula_09/src/aula_09.c
+++ aula_09/src/aula_09.c
@@ -10,6 +10,8 @@ int main(){
 
     srand(time(NULL));  //initialize a random seed
 
+    //Variables definition and initalization
+
     char guessWord [] [16] = {
         "green",
         "yellow",
@@ -30,6 +32,7 @@ int main(){
 
     int lengthOfWord = strlen(guessWord[randomIndex]);
 
+    //track the number of correct letters
     int letterGuessed[8] = { 0, 0, 0, 0, 0, 0, 0, 0};
 
     int quit = 0;
@@ -37,15 +40,18 @@ int main(){
     char guess[16];
     char letterEntered;
 
-    printf("The word is %s \n\n", guessWord[randomIndex]); //DEBUG
+    //DEBUG
+    printf("The word is %s \n\n", guessWord[randomIndex]);
 
     //game loop
     while( numCorrect < lengthOfWord){
         printf("Enter a guess letter:");
         printf("\nSo far number of correct letters are: %d\n", numCorrect);
 
+        //Read input from user
         fgets(guess,16, stdin);
 
+        //check for Quit the game
         if (strncmp(guess, "quit", 4) == 0){
             quit = 1;
             break;
@@ -57,29 +63,35 @@ int main(){
 
         oldCorrect = numCorrect;
 
+        //check the entered letter match any letter for the secret word
         for (indexLoop = 0; indexLoop < lengthOfWord; indexLoop++){
             if (letterGuessed[indexLoop] == 1){
                 continue;
             }
             if (letterEntered == guessWord[randomIndex][indexLoop]) {
+                //correct guess
                 letterGuessed[indexLoop] = 1;
                 numCorrect++;
             }
         }
+        //Decrement Live
         if (oldCorrect == numCorrect){
             numLives--;
             printf("Sorry, wrong guess!\n");
             if (numLives == 0){
+                //No more lives. Exit Game Loop
                 break;
                 }
         }   
             else {
+                //Correct letter
                 printf("Correct guess!\n");
             }
         
 
     }//while loop
 
+    //End of game (user quit, No more Lives or Win)
     if (quit == 1){
         printf("\nUser quit early.\n");
     } else if (numLives == 0){
